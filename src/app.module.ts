21ed067e34c2d5407afcc065f16bee7427e75209
idyukpong <idongesitukpong2020@gmail.com>
2024-11-03 18:00:24 +0100

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KnexModule } from './knex/knex.module'; 
 import { UserModule } from './user/user.module';
-import cors from 'cors'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [KnexModule, UserModule], 
+  imports: [KnexModule, UserModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    })
+  ], 
   controllers: [AppController],
   providers: [AppService],
 })
